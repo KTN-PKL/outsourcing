3e894c50c8a3a9d50bd151f9186751e3ae8f54d5
gusnanda7 <gus11111234@gmail.com>
2022-10-17 13:05:52 +0700

    migration

--- app/Models/lowongan.php
+++ app/Models/lowongan.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class lowongan extends Model
 {
     use HasFactory;
+
+    
 }
--- app/Models/perusahaan.php
+++ app/Models/perusahaan.php
@@ -4,8 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class perusahaan extends Model
 {
     use HasFactory;
+
+    public $table =
 }
