d75ca6dce05840509eba4e295dd2613fd2905544
gusnanda7 <gus11111234@gmail.com>
2022-10-18 09:16:06 +0700

    test

---
+++ resources/views/layouts/daftartemp.blade.php
@@ -0,0 +1,190 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html class="supernova"><head><script>console.warn("Server Side Rendering => render-from: frontend");</script>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<link rel="alternate" type="application/json+oembed" href="https://www.jotform.com/oembed/?format=json&amp;url=https%3A%2F%2Fform.jotform.com%2F222898833502463" title="oEmbed Form">
+<link rel="alternate" type="text/xml+oembed" href="https://www.jotform.com/oembed/?format=xml&amp;url=https%3A%2F%2Fform.jotform.com%2F222898833502463" title="oEmbed Form">
+<meta property="og:title" content="IT Service Ticket Form" >
+<meta property="og:url" content="https://form.jotform.com/222898833502463" >
+<meta property="og:description" content="Please click the link to complete this form." >
+<meta name="slack-app-id" content="AHNMASS8M">
+<link rel="shortcut icon" href="https://cdn.jotfor.ms/assets/img/favicons/favicon-2021.svg">
+<meta property="og:image" content="https://cdn.jotfor.ms/assets/img/favicons/favicon-2021.svg" />
+<link rel="canonical" href="https://form.jotform.com/222898833502463" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=2.0, user-scalable=1" />
+<meta name="HandheldFriendly" content="true" />
+<title></title>
+<!-- <style type="text/css">@media print{.form-section{display:inline!important}.form-pagebreak{display:none!important}.form-section-closed{height:auto!important}.page-section{position:initial!important}}</style> -->
+<link type="text/css" rel="stylesheet" href="https://cdn01.jotfor.ms/themes/CSS/5e6b428acc8c4e222d1beb91.css?themeRevisionID=5f7ed99c2c2c7240ba580251"/>
+<link type="text/css" rel="stylesheet" href="https://cdn02.jotfor.ms/css/styles/payment/payment_styles.css?3.3.36454" />
+<link type="text/css" rel="stylesheet" href="https://cdn03.jotfor.ms/css/styles/payment/payment_feature.css?3.3.36454" />
+<script src="https://cdn01.jotfor.ms/static/prototype.forms.js?3.3.36454" type="text/javascript"></script>
+<script src="https://cdn02.jotfor.ms/static/jotform.forms.js?3.3.36454" type="text/javascript"></script>
+<script defer src="https://cdnjs.cloudflare.com/ajax/libs/punycode/1.4.1/punycode.js"></script>
+<script src="https://cdn03.jotfor.ms/js/vendor/imageinfo.js?v=3.3.36454" type="text/javascript"></script>
+<script src="https://cdn01.jotfor.ms/file-uploader/fileuploader.js?v=3.3.36454"></script>
+<script type="text/javascript">	JotForm.newDefaultTheme = true;
+	JotForm.extendsNewTheme = false;
+	JotForm.singleProduct = false;
+	JotForm.newPaymentUIForNewCreatedForms = false;
+	JotForm.newPaymentUI = true;
+
+	JotForm.init(function(){
+	/*INIT-START*/
+      setTimeout(function() {
+          $('input_5').hint('ex: myname@example.com');
+       }, 20);
+if (window.JotForm && JotForm.accessible) $('input_4').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_8').setAttribute('tabindex',0);
+if (window.JotForm && JotForm.accessible) $('input_6').setAttribute('tabindex',0);
+      JotForm.setCustomHint( 'input_6', 'Type here...' );
+      setTimeout(function() {
+          JotForm.initMultipleUploads();
+      }, 2);
+	/*INIT-END*/
+	});
+
+   JotForm.prepareCalculationsOnTheFly([null,{"name":"clickTo","qid":"1","text":"IT Service Ticket","type":"control_head"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"name","qid":"3","text":"Name","type":"control_fullname"},{"name":"department","qid":"4","text":"Department","type":"control_textbox"},{"name":"email5","qid":"5","subLabel":"example@example.com","text":"E-mail","type":"control_email"},{"name":"describeThe","qid":"6","text":"Describe the Problem","type":"control_textarea"},{"name":"uploadScreenshot","qid":"7","text":"Upload Screenshot","type":"control_fileupload"},{"name":"computerId","qid":"8","text":"Computer ID","type":"control_textbox"}]);
+   setTimeout(function() {
+JotForm.paymentExtrasOnTheFly([null,{"name":"clickTo","qid":"1","text":"IT Service Ticket","type":"control_head"},{"name":"submit","qid":"2","text":"Submit","type":"control_button"},{"name":"name","qid":"3","text":"Name","type":"control_fullname"},{"name":"department","qid":"4","text":"Department","type":"control_textbox"},{"name":"email5","qid":"5","subLabel":"example@example.com","text":"E-mail","type":"control_email"},{"name":"describeThe","qid":"6","text":"Describe the Problem","type":"control_textarea"},{"name":"uploadScreenshot","qid":"7","text":"Upload Screenshot","type":"control_fileupload"},{"name":"computerId","qid":"8","text":"Computer ID","type":"control_textbox"}]);}, 20); 
+</script>
+</head>
+<body>
+<form class="jotform-form" action="https://submit.jotform.com/submit/222898833502463/" method="post" enctype="multipart/form-data" name="form_222898833502463" id="222898833502463" accept-charset="utf-8" autocomplete="on">
+  <input type="hidden" name="formID" value="222898833502463" />
+  <input type="hidden" id="JWTContainer" value="" />
+  <input type="hidden" id="cardinalOrderNumber" value="" />
+  <div role="main" class="form-all">
+    <style>
+      .form-all:before { background: none;}
+    </style>
+    <ul class="form-section page-section">
+      <li id="cid_1" class="form-input-wide" data-type="control_head">
+        <div class="form-header-group  header-large">
+          <div class="header-text httal htvam">
+            <h1 id="header_1" class="form-header" data-component="header">
+              IT Service Ticket
+            </h1>
+            <div id="subHeader_1" class="form-subHeader">
+              Please provide the details of the problem
+            </div>
+          </div>
+        </div>
+      </li>
+      <li class="form-line form-line-column form-col-1" data-type="control_fullname" id="id_3">
+        <label class="form-label form-label-top form-label-auto" id="label_3" for="first_3"> Name </label>
+        <div id="cid_3" class="form-input-wide" data-layout="full">
+          <div data-wrapper-react="true">
+            <span class="form-sub-label-container" style="vertical-align:top" data-input-type="first">
+              <input type="text" id="first_3" name="q3_name[first]" class="form-textbox" data-defaultvalue="" autoComplete="section-input_3 given-name" size="10" value="" data-component="first" aria-labelledby="label_3 sublabel_3_first" />
+              <label class="form-sub-label" for="first_3" id="sublabel_3_first" style="min-height:13px" aria-hidden="false"> First Name </label>
+            </span>
+            <span class="form-sub-label-container" style="vertical-align:top" data-input-type="last">
+              <input type="text" id="last_3" name="q3_name[last]" class="form-textbox" data-defaultvalue="" autoComplete="section-input_3 family-name" size="15" value="" data-component="last" aria-labelledby="label_3 sublabel_3_last" />
+              <label class="form-sub-label" for="last_3" id="sublabel_3_last" style="min-height:13px" aria-hidden="false"> Last Name </label>
+            </span>
+          </div>
+        </div>
+      </li>
+      <li class="form-line form-line-column form-col-2" data-type="control_email" id="id_5">
+        <label class="form-label form-label-top form-label-auto" id="label_5" for="input_5"> E-mail </label>
+        <div id="cid_5" class="form-input-wide" data-layout="half">
+          <span class="form-sub-label-container" style="vertical-align:top">
+            <input type="email" id="input_5" name="q5_email5" class="form-textbox validate[Email]" data-defaultvalue="" style="width:310px" size="310" value="" placeholder="ex: myname@example.com" data-component="email" aria-labelledby="label_5 sublabel_input_5" />
+            <label class="form-sub-label" for="input_5" id="sublabel_input_5" style="min-height:13px" aria-hidden="false"> example@example.com </label>
+          </span>
+        </div>
+      </li>
+      <li class="form-line form-line-column form-col-3" data-type="control_textbox" id="id_4">
+        <label class="form-label form-label-top form-label-auto" id="label_4" for="input_4"> Department </label>
+        <div id="cid_4" class="form-input-wide" data-layout="half">
+          <input type="text" id="input_4" name="q4_department" data-type="input-textbox" class="form-textbox" data-defaultvalue="" style="width:310px" size="310" value="" placeholder=" " data-component="textbox" aria-labelledby="label_4" />
+        </div>
+      </li>
+      <li class="form-line form-line-column form-col-4" data-type="control_textbox" id="id_8">
+        <label class="form-label form-label-top form-label-auto" id="label_8" for="input_8"> Computer ID </label>
+        <div id="cid_8" class="form-input-wide" data-layout="half">
+          <input type="text" id="input_8" name="q8_computerId" data-type="input-textbox" class="form-textbox" data-defaultvalue="" style="width:310px" size="310" value="" placeholder=" " data-component="textbox" aria-labelledby="label_8" />
+        </div>
+      </li>
+      <li class="form-line" data-type="control_fileupload" id="id_7">
+        <label class="form-label form-label-top form-label-auto" id="label_7" for="input_7"> Upload Screenshot </label>
+        <div id="cid_7" class="form-input-wide" data-layout="full">
+          <div class="jfQuestion-fields" data-wrapper-react="true">
+            <div class="jfField isFilled">
+              <div class="jfUpload-wrapper">
+                <div class="jfUpload-container">
+                  <div class="jfUpload-text-container">
+                    <div class="jfUpload-icon forDesktop">
+                      <span class="iconSvg  dhtupload ">
+                        function SvgDhtupload(props) { return /* @__PURE__ */ react.createElement("svg", dhtupload_svg_extends({ width: 54, height: 47, xmlns: "http://www.w3.org/2000/svg" }, props), dhtupload_svg_path || (dhtupload_svg_path = /* @__PURE__ */ react.createElement("path", { d: "M40.213 10.172c1.897.21 3.68.738 5.35 1.58a15.748 15.748 0 014.374 3.242 15.065 15.065 0 012.951 4.533c.72 1.704 1.08 3.522 1.08 5.455 0 1.827-.28 3.654-.843 5.48-.562 1.828-1.379 3.47-2.45 4.929A13.39 13.39 0 0146.669 39c-1.599.948-3.452 1.458-5.56 1.528H37.26a1.62 1.62 0 01-1.185-.5 1.62 1.62 0 01-.501-1.186c0-.457.167-.852.5-1.186.334-.334.73-.5 1.186-.5h3.848c1.44 0 2.75-.37 3.926-1.108a10.851 10.851 0 003.03-2.846 13.53 13.53 0 001.95-3.9 14.23 14.23 0 00.686-4.321c0-1.582-.316-3.066-.949-4.454a11.623 11.623 0 00-2.582-3.636 12.857 12.857 0 00-3.742-2.478 11.054 11.054 0 00-4.48-.922l-1.212-.053-.37-1.159c-.878-2.81-2.292-4.998-4.242-6.562-1.95-1.563-4.594-2.345-7.932-2.345-2.108 0-4.005.36-5.692 1.08-1.686.72-3.136 1.722-4.348 3.005-1.212 1.282-2.143 2.81-2.793 4.585-.65 1.774-.975 3.68-.975 5.718h.053l.105 1.581-1.528.264c-1.863.316-3.444 1.317-4.744 3.004-1.3 1.686-1.95 3.584-1.95 5.692 0 2.39.8 4.462 2.398 6.219 1.599 1.757 3.488 2.635 5.666 2.635h4.849c.492 0 .896.167 1.212.5.316.335.474.73.474 1.187 0 .456-.158.852-.474 1.185-.316.334-.72.501-1.212.501h-4.849a10.08 10.08 0 01-4.374-.975 11.673 11.673 0 01-3.61-2.661 13.173 13.173 0 01-2.478-3.9A12.073 12.073 0 010 28.301c0-2.706.755-5.148 2.266-7.326 1.511-2.178 3.444-3.636 5.798-4.374.14-2.354.658-4.542 1.554-6.562.896-2.02 2.091-3.777 3.584-5.27 1.494-1.494 3.25-2.662 5.27-3.505C20.493.422 22.733 0 25.193 0c1.898 0 3.637.237 5.218.711 1.581.475 3.004 1.151 4.269 2.03a13.518 13.518 0 013.268 3.215 18.628 18.628 0 012.266 4.216zm-11.964 13.44l6.22 6.85c.245.247.368.537.368.87 0 .334-.123.642-.369.923l-.421.263c-.211.246-.484.343-.817.29a1.544 1.544 0 01-.87-.448l-3.69-4.11v16.97c0 .492-.166.896-.5 1.212-.334.316-.729.474-1.186.474-.492 0-.896-.158-1.212-.474-.316-.316-.474-.72-.474-1.212V28.25l-3.584 4.005a1.544 1.544 0 01-.87.448.959.959 0 01-.87-.29l-.42-.264c-.247-.28-.37-.588-.37-.922 0-.334.123-.624.37-.87l6.113-6.746v-.052l.421-.422a.804.804 0 01.396-.29c.158-.053.307-.079.448-.079.175 0 .333.026.474.079.14.053.281.15.422.29l.421.422v.052z", fill: "none" }))); }
+                      </span>
+                    </div>
+                  </div>
+                  <div class="jfUpload-button-container">
+                    <div class="jfUpload-button" aria-hidden="true" tabindex="0" style="display:none" data-version="v2">
+                      Upload a File
+                      <div class="jfUpload-heading forDesktop">
+                        Drag and drop files here
+                      </div>
+                      <div class="jfUpload-heading forMobile">
+                        Choose a file
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="jfUpload-files-container">
+                  <input type="file" id="input_7" name="q7_uploadScreenshot[]" multiple="" class="form-upload-multiple" data-imagevalidate="yes" data-file-accept="pdf, doc, docx, xls, csv, txt, rtf, html, zip, mp3, wma, mpg, flv, avi, jpg, jpeg, png, gif" data-file-maxsize="10240" data-file-minsize="0" data-file-limit="0" data-component="fileupload" aria-label="Upload a File" />
+                </div>
+              </div>
+              <div data-wrapper-react="true">
+              </div>
+            </div>
+            <span style="display:none" class="cancelText">
+              Cancel
+            </span>
+            <span style="display:none" class="ofText">
+              of
+            </span>
+          </div>
+        </div>
+      </li>
+      <li class="form-line" data-type="control_textarea" id="id_6">
+        <label class="form-label form-label-top form-label-auto" id="label_6" for="input_6"> Describe the Problem </label>
+        <div id="cid_6" class="form-input-wide" data-layout="full">
+          <textarea id="input_6" class="form-textarea" name="q6_describeThe" style="width:648px;height:163px" data-component="textarea" aria-labelledby="label_6"></textarea>
+        </div>
+      </li>
+      <li class="form-line" data-type="control_button" id="id_2">
+        <div id="cid_2" class="form-input-wide" data-layout="full">
+          <div data-align="auto" class="form-buttons-wrapper form-buttons-auto   jsTest-button-wrapperField">
+            <button id="input_2" type="submit" class="form-submit-button submit-button jf-form-buttons jsTest-submitField" data-component="button" data-content="">
+              Submit
+            </button>
+          </div>
+        </div>
+      </li>
+      <li style="display:none">
+        Should be Empty:
+        <input type="text" name="website" value="" />
+      </li>
+    </ul>
+  </div>
+  <script>
+  JotForm.showJotFormPowered = "new_footer";
+  </script>
+  <script>
+  JotForm.poweredByText = "Powered by Jotform";
+  </script>
+  <input type="hidden" class="simple_spc" id="simple_spc" name="simple_spc" value="222898833502463" />
+  <script type="text/javascript">
+  var all_spc = document.querySelectorAll("form[id='222898833502463'] .si" + "mple" + "_spc");
+for (var i = 0; i < all_spc.length; i++)
+{
+  all_spc[i].value = "222898833502463-222898833502463";
+}
+  </script>
+  
+</form></body>
+</html>
+<script type="text/javascript">JotForm.ownerView=true;</script><script src="https://cdn.jotfor.ms//js/vendor/smoothscroll.min.js?v=3.3.36454"></script>
+<script src="https://cdn.jotfor.ms//js/errorNavigation.js?v=3.3.36454"></script>
