333abbd69fc4226f155272b8c2fd9110c9904d61
bagaas86 <bagaas86@gmail.com>
2022-10-20 13:52:29 +0700

    view

--- resources/views/user/v_daftarperusahaan.blade.php
+++ resources/views/user/v_daftarperusahaan.blade.php
@@ -5,16 +5,18 @@
 <h2>DAFTAR PERUSAHAAN</h2>
 <div class="jumbotron p-5 rounded-3">
     <div class="container py-4">
-            <div class="col-sm-6">
+            <div class="col-md-6">
                 
                 <div class="row">
                     @foreach($perusahaan as $perusahaans)
-                  <div class="col-md-6 mb-6">
-                    <div class="card shadow col-md-12" style="margin-left: 1em; margin-top:1em;">
+                  <div class="col-md-6 col-xs-3">
+                    <div class="card">
                         <br>
                         <img src="./template2/landingpage/img/ktn.png" alt="">
-                                 <div class="card-body" style="width:18rem">
-                             <h6>{{$perusahaans->name}}</h6> <br>
+                                 <div class="card-body">
+                             <h6 class="card-title">{{$perusahaans->name}}</h6>
+                             <br>
+                             <br>
                             <i class="fa fa-map-marker-alt me-3 mt-3" aria-hidden="true">{{$perusahaans->alamat}}</i><br>
                                    </div>
                     </div>
