27725abccdca9109e72ab446ec21699b7e0da493
bagaas86 <bagaas86@gmail.com>
2022-10-20 11:57:24 +0700

    viwe

--- resources/views/user/v_daftarperusahaan.blade.php
+++ resources/views/user/v_daftarperusahaan.blade.php
@@ -1,4 +1,30 @@
 @extends('layouts.v_templateregister')
-@section('content1')  
+@section('content1') 
+<br>
+<br> 
+<h2>DAFTAR PERUSAHAAN</h2>
+<div class="jumbotron p-5 rounded-3">
+    <div class="container py-4">
+        <div class="row">
+            <div class="col-sm-6">
+                
+                <div class="row">
+                    {{-- @foreach($lowongan as $lowongans) --}}
+                  <div class="col-md-4 mb-3">
+                    <div class="card shadow" style="margin-left: 1em; margin-top:1em;">
+                                 <div class="card-body" style="width:12rem">
+                             <p><img height="30px" width="60px" src="#" alt="logo">CV Kreasi Teknologi Nusantara</p> <br>
+                            <i class="fa fa-map-marker-alt me-3 mt-3" aria-hidden="true">Subang</i><br>
+                                   </div>
+                    </div>
+                  </div>   
+                
+              </div>
+              
+              
+            </div>
+          </div>
+    </div>
 
+</div>
 @endsection
