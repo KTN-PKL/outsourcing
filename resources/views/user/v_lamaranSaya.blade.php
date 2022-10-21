5036d0139e02defbc1b21e243395eb1795787612
bagaas86 <bagaas86@gmail.com>
2022-10-21 10:11:59 +0700

    lamaran

--- resources/views/user/v_lamaranSaya.blade.php
+++ resources/views/user/v_lamaranSaya.blade.php
@@ -2,10 +2,32 @@
 @section('content1') 
 <br>
 <br> 
+<br>
 <h2>LAMARAN SAYA</h2>
 <div class="jumbotron p-5 rounded-3">
     <div class="container py-4">
-            <div class="col-md-6">
+        <div style="width:600px;" class="card">
+            <table style="width:600px" class="table table-bordered table-hover">
+              <tr>
+                <th>No</th>
+                <th>Perusahaan</th>
+                <th>Posisi</th>
+              </tr>
+              @foreach($lamaran as $lamarans)
+              <tr>
+              <td>1</td>
+              <td>{{$lamarans->nama}}</td>
+              <td>{{$lamarans->posisi}}</td>
+            </tr>
+              @endforeach        
+            </table>
+          
+          </div>
+                    </div>
+                    
+                </div>
+            </table>
+             
 
             </div>
     </div>
