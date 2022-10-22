858978cf10c5072b6423b4e274e2a5ebe0b0dc43
gusnanda7 <gus11111234@gmail.com>
2022-10-22 10:40:53 +0700

    comit

--- resources/views/perusahaan/v_perusahaan.blade.php
+++ resources/views/perusahaan/v_perusahaan.blade.php
@@ -10,17 +10,16 @@ Portal Kerja
 
 <br>
 <br>
-<div class="row">
-<div style="width:40%;" class="card">
+<br>
+<div class="card col-md-2 offset-md-5">
   <div class="icon-demo d-flex align-items-center justify-content-center p-3 py-6">
     <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
       <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
     </svg>
     <i class="bi bi-person-fill"></i>
     </div>
-    <p>Pelamar</p>
-    <p>{{ $pelamar }}</p>
+    <p style="text-align: center">Pelamar</p>
+    <p style="text-align: center">{{ $pelamar }}</p>
   </div>
-</div>
 </div>
 @endsection
