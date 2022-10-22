024569bfc80c7f8b111dd9f9aa713e054faaa447
bagaas86 <bagaas86@gmail.com>
2022-10-22 10:53:18 +0700

    view

--- resources/views/user/v_daftarperusahaan.blade.php
+++ resources/views/user/v_daftarperusahaan.blade.php
@@ -2,25 +2,25 @@
 @section('content1') 
 <br>
 <br> 
+<div class="container">
 <h2>DAFTAR PERUSAHAAN</h2>
-<div class="jumbotron p-5 rounded-3">
-    <div class="container py-4">
-            <div class="col-md-6">
+{{-- <div class="jumbotron p-5 rounded-3"> --}}
+  
+            <div class="col-md-12">
                 
                 <div class="row">
                     @foreach($perusahaan as $perusahaans)
-                  <div class="col-md-6 col-xs-3">
+                  <div class="col-md-3 ">
                     <div class="card">
-                        <br>
-                        <img src="./template2/landingpage/img/ktn.png" alt="">
-                                 <div class="card-body">
-                             <h6 class="card-title">{{$perusahaans->name}}</h6>
-                             <br>
-                             <br>
-                            <i class="fa fa-map-marker-alt me-3 mt-3" aria-hidden="true">{{$perusahaans->alamat}}</i><br>
-                                   </div>
-                    </div>
-                        </div>  
+                        <img src="{{asset('/logo/'. $perusahaans->logo)}}" width="250" height="200" style="display:block; margin:auto;" alt="">
+                        </div>
+                            <div class="card-header">
+                                <h6 class="card-title">{{$perusahaans->name}}</h6>
+                                <br>
+                                <br>
+                                <i class="fa fa-map-marker-alt me-3 mt-3" aria-hidden="true">{{$perusahaans->alamat}}</i><br>
+                            </div>
+                    </div>  
                         @endforeach 
                 
               </div>
