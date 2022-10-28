6d8e8725f919692df16a09476a10143fb022b3ce
bagaas86 <bagaas86@gmail.com>
2022-10-28 10:00:38 +0700

    vieww

--- resources/views/perusahaan/v_perusahaan.blade.php
+++ resources/views/perusahaan/v_perusahaan.blade.php
@@ -8,7 +8,69 @@ Portal Kerja
 
 @section('content')
 <div class="container" >
-  @if(auth()->user()->status==1)
+  @if(auth()->user()->status==null)
+  <div class="row">
+    <div class="col-md-12">
+        <div class="card">
+<div class="modal-header">
+<h3><b>Form Verifikasi Perusahaan</b></h3>
+</div>
+<form method="POST" action="{{ route('perusahaan.verifikasi') }}">
+@csrf
+<div style="margin-left:2em;margin-right:2em;margin-top:1em" class="mb-3">
+  <label class="form-label">NPWP</label>
+  <input type="text" class="form-control @error('npwp') is-invalid @enderror" value="{{ old('npwp') }}" name="npwp" placeholder="Masukkan Nomor Pokok Wajib Pajak">
+  @error('npwp')
+  <span class="invalid-feedback" role="alert">
+    <strong>{{ $message }}</strong>
+  </span>
+@enderror
+</div>
+
+<div style="margin-left:2em;margin-right:2em;margin-top:1em" class="mb-3">
+<label class="form-label">NIB</label>
+<input type="text" class="form-control @error('posisi') is-invalid @enderror" value="{{ old('nib') }}" name="nib" placeholder="Masukkan Nomor Induk Berusaha">
+@error('nib')
+<span class="invalid-feedback" role="alert">
+  <strong>{{ $message }}</strong>
+</span>
+@enderror
+</div>
+
+
+            <div style="margin-left:2em;margin-right:2em" class="mb-3">
+              <!-- Upload PDF input-->
+              <label class="form-label">Akte Pendirian</label>
+                  <input type="file" onchange="readURL(this);" class="form-control"  name="akta">
+          
+                      </div>
+
+           <div style="margin-left:2em;margin-right:2em" class="mb-3">
+            <!-- Upload image input-->
+            <label class="form-label"> Foto Kantor </label>
+                <input type="file" onchange="readURL(this);" class="form-control"  name="fotokantor">
+         
+                     </div>       
+                     
+               <div style="margin-left:2em;margin-right:2em" class="mb-3">
+                      <!-- Upload image input-->
+                      <label class="form-label"> PKP (Opsional) </label>
+                          <input type="file" onchange="readURL(this);" class="form-control"  name="pkp">
+                   
+                               </div> 
+                               
+                               <div class="row">
+                                <div class="col-md-12 offset-md-5">
+                                    <button style="margin-bottom: 2em;padding:10px 24px;" type="submit" class="btn btn-primary">
+                                        {{ __('Kirim') }}
+                                    </button>
+                                </div>
+                            </div>
+        </div>
+    </div>
+</div>
+  @else
+  @if ((auth()->user()->status==1))
   <div class="card-header col-md-2 offset-md-5">   
     <div class="icon-demo d-flex align-items-center justify-content-center p-3 py-6">
       <svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
@@ -19,69 +81,9 @@ Portal Kerja
       <p style="text-align: center">Pelamar</p>
       <p style="text-align: center">{{ $lamaran }}</p>
   </div>
-</div>
   @else
-<div class="container">
-  <div class="row">
-      <div class="col-md-12">
-          <div class="card">
-<div class="modal-header">
-  <h3><b>Form Verifikasi Perusahaan</b></h3>
-</div>
-<form method="POST" action="{{ route('perusahaan.verifikasi') }}">
-  @csrf
-  <div style="margin-left:2em;margin-right:2em;margin-top:1em" class="mb-3">
-    <label class="form-label">NPWP</label>
-    <input type="text" class="form-control @error('npwp') is-invalid @enderror" value="{{ old('npwp') }}" name="npwp" placeholder="Masukkan Nomor Pokok Wajib Pajak">
-    @error('npwp')
-    <span class="invalid-feedback" role="alert">
-      <strong>{{ $message }}</strong>
-    </span>
-@enderror
-</div>
-
-<div style="margin-left:2em;margin-right:2em;margin-top:1em" class="mb-3">
-  <label class="form-label">NIB</label>
-  <input type="text" class="form-control @error('posisi') is-invalid @enderror" value="{{ old('nib') }}" name="nib" placeholder="Masukkan Nomor Induk Berusaha">
-  @error('nib')
-  <span class="invalid-feedback" role="alert">
-    <strong>{{ $message }}</strong>
-  </span>
-@enderror
-</div>
-
-
-              <div style="margin-left:2em;margin-right:2em" class="mb-3">
-                <!-- Upload PDF input-->
-                <label class="form-label">Akte Pendirian</label>
-                    <input type="file" onchange="readURL(this);" class="form-control"  name="akta">
-            
-                        </div>
-
-             <div style="margin-left:2em;margin-right:2em" class="mb-3">
-              <!-- Upload image input-->
-              <label class="form-label"> Foto Kantor </label>
-                  <input type="file" onchange="readURL(this);" class="form-control"  name="fotokantor">
-           
-                       </div>       
-                       
-                 <div style="margin-left:2em;margin-right:2em" class="mb-3">
-                        <!-- Upload image input-->
-                        <label class="form-label"> PKP (Opsional) </label>
-                            <input type="file" onchange="readURL(this);" class="form-control"  name="pkp">
-                     
-                                 </div> 
-                                 
-                                 <div class="row">
-                                  <div class="col-md-12 offset-md-5">
-                                      <button style="margin-bottom: 2em;padding:10px 24px;" type="submit" class="btn btn-primary">
-                                          {{ __('Kirim') }}
-                                      </button>
-                                  </div>
-                              </div>
-          </div>
-      </div>
-  </div>
-</div> 
+      <h4>Anda Sedang dalam tahap verifikasi</h4>
+  @endif
 @endif
 @endsection
+
