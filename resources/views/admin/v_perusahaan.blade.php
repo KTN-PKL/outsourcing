c37a256369a78704807a6b80809f3dd788f8185c
bagaas86 <bagaas86@gmail.com>
2022-10-28 14:13:12 +0700

    committt

--- resources/views/admin/v_perusahaan.blade.php
+++ resources/views/admin/v_perusahaan.blade.php
@@ -66,7 +66,7 @@ Daftar Perusahaan
                   <!-- Modal Detail Perusahaan -->
                   @foreach ($perusahaan as $perusahaans)                  
                   <div class="modal fade" id="detail{{$perusahaans->id_perusahaan}}">
-                    <div class="modal-dialog modal-md-12">
+                    <div class="modal-dialog">
                         <div class="modal-content">
                             <div class="modal-header">
                                 <h6 class="modal-title">{{$perusahaans->nama}}</h6>
@@ -75,15 +75,31 @@ Daftar Perusahaan
                                 </button>
                             </div>
                             <div class="modal-body">
-                              <p>NPWP          : {{ $perusahaans->npwp }}</p>
-                              <p>NIB           : {{ $perusahaans->nib }}</p>
-                              {{$perusahaans->akta}}
-                              <div class="row">
-                              <p>File Dokumen  : <a href="{{ route('admin.perusahaan.downloadakta', $perusahaans->akta) }}" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
+                              <div class="col-md-4 offset-md-4">
+                              <img src="{{asset('./akta/'. $perusahaans->fotokantor)}}" width="150px" height="200px" alt="...">
+                              </div>  
+                              <table>
+                                <tr>
+                                  <td style="width: 100px"><b>NPWP</b></td>
+                                  <td>: {{$perusahaans->npwp}}</td>
+                                </tr>
+                                <tr>
+                                  <td style="width: 100px" ><b>NIB</b></td>
+                                  <td>: {{$perusahaans->nib}}</td>
+                                </tr>
+                              </table>
+                             
+                             <div class="row">
+                              <p>File Dokumen  : <a href="/admin/perusahaan/downloadakta/{{$perusahaans->akta}}" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
                               <path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z"/>
                               <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
-                              </svg>  Download Akta</a></p>
+                              </svg>  Download Akta</a>
+                              <a href="/admin/perusahaan/downloadpkp/{{$perusahaans->pkp}}" class="btn btn-primary"><svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-cloud-arrow-down" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd" d="M7.646 10.854a.5.5 0 0 0 .708 0l2-2a.5.5 0 0 0-.708-.708L8.5 9.293V5.5a.5.5 0 0 0-1 0v3.793L6.354 8.146a.5.5 0 1 0-.708.708l2 2z"/>
+                                <path d="M4.406 3.342A5.53 5.53 0 0 1 8 2c2.69 0 4.923 2 5.166 4.579C14.758 6.804 16 8.137 16 9.773 16 11.569 14.502 13 12.687 13H3.781C1.708 13 0 11.366 0 9.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383zm.653.757c-.757.653-1.153 1.44-1.153 2.056v.448l-.445.049C2.064 6.805 1 7.952 1 9.318 1 10.785 2.23 12 3.781 12h8.906C13.98 12 15 10.988 15 9.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 4.825 10.328 3 8 3a4.53 4.53 0 0 0-2.941 1.1z"/>
+                                </svg>  Download PKP</a></p>
                             </div>
+                           
                             <div class="modal-footer">
                                 <a href="{{ route('admin.perusahaan.verifikasi', $perusahaans->id) }}" class="btn btn-outline-success float-left">Verifikasi</a>
                                 <button type="button" class="btn btn-outline-danger" data-dismiss="modal">Batal Verifikasi</button>
