9debff2761972f64e2804ec9cac8c7a7ef9cbcf3
gusnanda7 <gus11111234@gmail.com>
2022-11-01 15:20:18 +0700

    com

--- resources/views/layouts/v_templateUser.blade.php
+++ resources/views/layouts/v_templateUser.blade.php
@@ -7,8 +7,8 @@
 <section class="content1">
       
 
-    <!-- Default box -->
-    @yield('content1')
+    {{-- <!-- Default box -->
+    @yield('content1') --}}
     
         <style> .container { width: 500px; padding: 0 0 5px 0; margin: 3px 0 10px 0; background: rgb(0, 0, 0); } #nav { margin: 0; padding: 0; border-top: 3px solid #000000; } #nav li { margin: 0; padding: 0; display: inline; list-style-type: none; } #nav a:link, #nav a:visited { float: left; font: bold 7.5pt verdana; line-height: 14px; padding: 9px; text-decoration: none; color: #708491; } #nav a:link.active, #nav a:visited.active, #nav a:hover { color: rgb(0, 0, 0); background: url(http://1.bp.blogspot.com/_7wsQzULWIwo/SzvC_Xa-vHI/AAAAAAAACw0/qVeZI3gdWQM/s1600/Inverted.png) no-repeat top center; border-top: 4px solid #000000; } </style> <div class="container">
             <div class="col-md-6 offset-md-4">
