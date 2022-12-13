08617180ba77e3481f915c3639053a61e74052cc
bagaas86 <bagaas86@gmail.com>
2022-12-13 13:34:50 +0700

    commit

--- resources/views/layouts/nav.blade.php
+++ resources/views/layouts/nav.blade.php
@@ -1,7 +1,7 @@
 <div class="container-fluid">
     <a class="navbar-brand" href="#"><h4>PORTAL KERJA</h4></a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button id="bars" onclick="klik()" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="fa fa-bars"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
@@ -65,3 +65,11 @@
     @endauth
     </div>
   </div>
+  <script>
+    function klik(){
+      document.getElementById('#bars')setAttribute('aria-expanded', 'false')
+    }
+  </script>
+
+  
+ 
